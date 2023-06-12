b6797b1db14da60ab7d8e0b8f1caed0639bde16b
Igor <igor.sakhankov@glovoapp.com>
2023-06-12 20:37:09 +0200

    feat(spring): initial commit

---
+++ src/main/kotlin/com/harbourspace/cloudcomputing/CloudcomputingApplication.kt
@@ -0,0 +1,11 @@
+package com.harbourspace.cloudcomputing
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CloudcomputingApplication
+
+fun main(args: Array<String>) {
+	runApplication<CloudcomputingApplication>(*args)
+}
